a2539e016cc6b6346750a1a37a6560417253daba
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-01 21:09:56 +0200

    Create fetch and update dom #14

---
+++ public/js/fetch.js
@@ -0,0 +1,8 @@
+const fetchData = (data) => {
+    const request = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ value: data.value }),
+    };
+    return fetch(`/search`, request);
+  };
