62e44d726070eeb06a452d0734c31efdfc5891b3
nada-abuzaid <nada.b.abu.zaid@gmail.com>
2022-03-01 21:30:24 +0200

    [JS] Create fetch and update app Relates #16

---
+++ src/controllers/fetch.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+
+const fetchNewsData = (request, response) => {
+  const endpoint = request.body.value;
+  console.log(endpoint);
+  return fetch(`https://www.reddit.com/r/${endpoint}.json`)
+    .then((resp) => resp.json())
+    .then((data) => response.json(data));
+};
+
+module.exports = fetchNewsData;
