1217724650a037f64905e5502fa73ebcf1fed454
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-01 20:43:56 +0200

    fix form #11

--- public/js/login.js
+++ public/js/login.js
@@ -7,37 +7,43 @@ const confirmPass = document.querySelector('#confirmPassword');
 const confirmPassErr = document.querySelector('#confirmErr');
 const submit = document.querySelector('button');
 
-form.addEventListener('submit', (e) => { submitHandler(e) });
+submit.addEventListener('click', (e) => {
+  submitHandler(e);
+});
 
 const submitHandler = (e) => {
-    if (emailErr.firstChild)
-        emailErr.removeChild(emailErr.firstChild);
-    if (passwordErr.firstChild)
-        passwordErr.removeChild(passwordErr.firstChild);
-    if (confirmPassErr.firstChild)
-        confirmPassErr.removeChild(confirmPassErr.firstChild);
-    email.style.borderColor = 'black';
-    password.style.borderColor = 'black';
-    confirmPass.style.borderColor = 'black';
-    if (email.value.length == 0) {
-        const err = document.createTextNode('PLEASE ENTER AN EMAIL ADDRESS');
-        emailErr.appendChild(err);
-        email.style.borderColor = 'red';
-        e.preventDefault();
-    } else if (!/^\w+@\w+\.\w+$/.test(email.value)) {
-        const err = document.createTextNode('PLEASE ENTER A VALID EMAIL ADDRESS');
-        emailErr.appendChild(err);
-        email.style.borderColor = 'red';
-        e.preventDefault();
-    } else if (password.value.length == 0) {
-        const err = document.createTextNode('PLEASE ENTER A PASSWORD');
-        passwordErr.appendChild(err);
-        password.style.borderColor = 'red';
-        e.preventDefault();
-    } else if (confirmPass.value.length == 0 || confirmPass.value != password.value) {
-        const err = document.createTextNode("PASSWORD YOU'VE ENTERED DO NOT MATCH");
-        confirmPassErr.appendChild(err);
-        confirmPass.style.borderColor = 'red';
-        e.preventDefault();
-    }
+  if (emailErr.firstChild) emailErr.removeChild(emailErr.firstChild);
+  if (passwordErr.firstChild) passwordErr.removeChild(passwordErr.firstChild);
+  if (confirmPassErr.firstChild)
+    confirmPassErr.removeChild(confirmPassErr.firstChild);
+  email.style.borderColor = 'black';
+  password.style.borderColor = 'black';
+  confirmPass.style.borderColor = 'black';
+  if (email.value.length == 0) {
+    const err = document.createTextNode('PLEASE ENTER AN EMAIL ADDRESS');
+    emailErr.appendChild(err);
+    email.style.borderColor = 'red';
+    e.preventDefault();
+  } else if (!/^\w+@\w+\.\w+$/.test(email.value)) {
+    const err = document.createTextNode('PLEASE ENTER A VALID EMAIL ADDRESS');
+    emailErr.appendChild(err);
+    email.style.borderColor = 'red';
+    e.preventDefault();
+  } else if (password.value.length == 0) {
+    const err = document.createTextNode('PLEASE ENTER A PASSWORD');
+    passwordErr.appendChild(err);
+    password.style.borderColor = 'red';
+    e.preventDefault();
+  } else if (
+    confirmPass.value.length == 0 ||
+    confirmPass.value != password.value
+  ) {
+    const err = document.createTextNode("PASSWORD YOU'VE ENTERED DO NOT MATCH");
+    confirmPassErr.appendChild(err);
+    confirmPass.style.borderColor = 'red';
+    e.preventDefault();
+  }
+// if(confirmPass.value == password.value){
+//   window.open('html/main.html', '_self')
+// }
 };
